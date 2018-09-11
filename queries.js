995fa2252d03dd9cb09f584824d2b79bfe047122
Cameron Hatch <cameronlavigne92@gmail.com>
2018-09-11 10:19:44 -0500

    question 2

--- queries.js
+++ queries.js
@@ -6,8 +6,15 @@ const knex = require('knex')(DATABASE);
 // clear the console (just a convenience)
 process.stdout.write('\x1Bc');
 
-// Sample select
+// Q1
+// knex
+//   .select()
+//   .from('restaurants')
+//   .then(results => console.log(results));
+
+// Q2
 knex
   .select()
   .from('restaurants')
+  .where({cuisine: 'Italian'})
   .then(results => console.log(results));
